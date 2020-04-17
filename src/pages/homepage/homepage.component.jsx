435a4492ca6128a2a188b83150af0685294ba9d9
Botebota <chalermkhwan.b.n@gmail.com>
2020-04-17 14:14:21 -0500

    broken homepage into smaller components and styled menu

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Directory from '../../components/directotry/directory.component';
+import MenuItem from '../../components/menu-item/menu-item';
+
+import './homepage.styles.scss';
+const Homepage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
+
+export default Homepage;
